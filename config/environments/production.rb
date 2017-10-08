48c0397c2d06787eabf6facaee5295579c667f1a
ShintaroKoizumi <shintakoizu@gmail.com>
2017-10-09 00:48:05 +0900

    heroku create after add mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '' }
+  config.action_mailer.default_url_options = { host: 'shrouded-chamber-77347' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  domain: "example.com",
+  domain: "heroku.com",
   address: "smtp.SendGrid.net",
   port: 587,
   authentication: :plain,
