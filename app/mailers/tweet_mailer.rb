6de3281bbdefc4ced3c7b71207aa157e027e8584
ShintaroKoizumi <shintakoizu@gmail.com>
2017-10-10 18:58:18 +0900

    /tweetmailer #mail to2

--- app/mailers/tweet_mailer.rb
+++ app/mailers/tweet_mailer.rb
@@ -1,7 +1,8 @@
 class TweetMailer < ApplicationMailer
   def tweet_mail(tweet)
     @tweet = tweet
+    @user =
 
-    mail to: @user.email, subject: "つぶやき通知"
+    mail to: user.email, subject: "つぶやき通知"
   end
 end
