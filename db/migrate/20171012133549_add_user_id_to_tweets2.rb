7b36a3b6ade2d0e451d342cc23d544c30e6f8bc2
ShintaroKoizumi <shintakoizu@gmail.com>
2017-10-14 14:46:18 +0900

    migration修正

--- db/migrate/20171012133549_add_user_id_to_tweets2.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToTweets2 < ActiveRecord::Migration[5.1]
-  def change
-    add_column :tweets, :user_id, :int8, limit:8
-  end
-end
--- db/migrate/20171012151234_remove_user_id_to_tweets2.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdToTweets2 < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :tweets, :user_id, :int8, limit:8
-  end
-end
